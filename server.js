c20d49a5cbab29567a5514550a70963f4464aba6
Leonar Gharib <leonargharib@gmail.com>
2018-06-21 13:37:59 -0700

    Clients can see other client messages, added help command

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ wss.on('connection', function connection(ws) {
   console.log("Connected to server");
   ws.id = makeid();
   const welcomeMsg = {
-    type: "message",
-    text: "Welcome to Leo's chat room, " + ws.id,
+    type: "server-message",
+    text: "Welcome to Leo's chat room, " + ws.id +". Type \'!help\' for help",
   };
   ws.send(JSON.stringify(welcomeMsg));
   const serverTime = {
@@ -32,18 +32,39 @@ wss.on('connection', function connection(ws) {
 		const userMsg = JSON.parse(msg);
 		if(userMsg.type=="message"){
 			userMsg.text = ws.id+": "+userMsg.text;
-			ws.send(JSON.stringify(userMsg));
+      wss.clients.forEach((client)=>{
+        client.send(JSON.stringify(userMsg));
+      });
 		}
+    //command handling
 		else if(userMsg.type=="command"){
 			console.log("got command");
 			const command = userMsg.text;
 			console.log("command text:" + userMsg.text);
+      //changename
 			if(command.substring(0,"changename".length)=="changename"){
 				ws.id=command.substring("changename".length);
+        ws.send(JSON.stringify(createMsg("server-message","Changed name to: " + ws.id)));
 			}
+      //help
+      else if(command.substring(0,"help".length)=="help"){
+        console.log("recognized help command");
+        const helpMsg = {
+          type: "server-message",
+          text: "Type \'!changename <some_name>\' to change your name!",
+        };
+        ws.send(JSON.stringify(helpMsg));
+      }
 		}
   });
 });
+function createMsg(msgType,msgText){
+  let msg = {
+    type: msgType,
+    text: msgText
+  }
+  return msg;
+}
 
 // send time to clients
 setInterval(() => {
