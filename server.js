9c40748de46db454fa3007d1021e4f49b7d2be02
Leonar Gharib <leonargharib@gmail.com>
2018-06-22 22:37:45 -0700

    Worked on css to scale site; changed random username generation; fixed bug allowing users to change usernames to in-use names; added blog

--- server.js
+++ server.js
@@ -3,93 +3,138 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const stream = require('stream');
+
+// for username generation
+var names = [];
+var adjectives = [];
+var usernames = [];
+
+
+// reads names and adjectives files and stores in arrays
+(function(){
+  let instream = fs.createReadStream('./names.txt');
+  const outstream = new stream;
+  let rl = readline.createInterface(instream, outstream);
+
+
+  rl.on('line',function(line){
+    names.push(line);
+  });
+
+  instream = fs.createReadStream('./adjectives.txt');
+  rl = readline.createInterface(instream, outstream);
+
+  rl.on('line',function(line){
+    adjectives.push(line);
+  });
+})();
 
 const PORT = process.env.PORT || 3000;
+//var server = express();
 
-const server = express()
-  .use(express.static(__dirname))
-  .use(express.static('public'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 /*
-var server = express();
-
-//server.use(express.static(__dirname+'/public'));
-server.use((req,res)=>res.sendFile(INDEX))
-.listen(PORT);
+server.use(express.static(__dirname))
+      .use(express.static('public'))
+      .use('/blog', function(req,res,next){
+        res.sendFile('blog.html');
+      })
+      .listen(PORT, ()=>console.log('Listening on %d',PORT));
 */
 
-const wss = new SocketServer({ server });
+  const server = express()
+.use(express.static(__dirname))
+  .use(express.static('public'))
+  .use('/blog', function(req,res,next){
+    res.sendFile(__dirname+ '/blog.html');
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-var clients = [];
+  const wss = new SocketServer({ server });
 
-wss.on('connection', function connection(ws) {
-  ws.id = makeid();
-  console.log("%s Connected to server",ws.id);
-  ws.isAlive = true;
-  ws.on('pong', ()=>{ws.isAlive=true});
-  const welcomeMsg = {
-    type: "server-message",
-    text: "Welcome to Leo's chat room, " + ws.id +". Type \'!help\' for help",
-  };
-  ws.send(JSON.stringify(welcomeMsg));
-  sendAllExcept("server-message",ws.id + " joined the chat room!",ws.id);
-  const serverTime = {
+  var clients = [];
+
+  wss.on('connection', function connection(ws) {
+    ws.id = makeUsername();
+    console.log("%s Connected to server",ws.id);
+    ws.isAlive = true;
+    ws.on('pong', ()=>{ws.isAlive=true});
+    const welcomeMsg = {
+      type: "server-message",
+      text: "Welcome to Leo's chat room, " + ws.id +". Type \'!help\' for help",
+    };
+    ws.send(JSON.stringify(welcomeMsg));
+    sendAllExcept("server-message",ws.id + " joined the chat room!",ws.id);
+    const serverTime = {
       type: "server-time",
       text: new Date().toTimeString()
-  }
-  ws.send(JSON.stringify(serverTime));
-
-  clients.push(ws);
-
-	// message from client
-  ws.on('message',function(msg){
-		const userMsg = JSON.parse(msg);
-		if(userMsg.type=="message"){
-			userMsg.text = ws.id+": "+userMsg.text;
-
-      wss.clients.forEach((client)=>{
-        console.log("going thru clients");
-        if(client==ws){
-          userMsg.selfMsg = true;
-          console.log("same client");
-        }
-        else userMsg.selfMsg = false;
-        client.send(JSON.stringify(userMsg));
-      });
-		}
-    //command handling
-		else if(userMsg.type=="command"){
-			const command = userMsg.text;
-      //changename
-			if(command.substring(0,"changename".length)=="changename"){
-				ws.id=command.substring("changename".length);
-        ws.send(JSON.stringify(createMsg("server-message","Changed name to: " + ws.id)));
-			}
-      //help
-      else if(command.substring(0,"help".length)=="help"){
-        const helpMsg = {
-          type: "server-message",
-          text: "Type \'!changename <some_name>\' to change your name! Type \'!users\' for a list of connected users",
-        };
-        ws.send(JSON.stringify(helpMsg));
-      }
-      //users
-      else if(command.substring(0,"users".length)=="users"){
-        let userList="";
-        wss.clients.forEach((client)=>{
-          if(userList.length>0)
-            userList += ", "+client.id;
-          else userList += client.id;
-        });
-        const usersMsg = {
-          type: "server-message",
-          text: "userList: " + userList
-        };
-        ws.send(JSON.stringify(usersMsg));
-      }
     }
+    ws.send(JSON.stringify(serverTime));
+
+    clients.push(ws);
+
+    // message from client
+    ws.on('message',function(msg){
+      const userMsg = JSON.parse(msg);
+      if(userMsg.type=="message"){
+        userMsg.text = ws.id+": "+userMsg.text;
+
+        wss.clients.forEach((client)=>{
+          if(client==ws){
+            userMsg.selfMsg = true;
+          }
+          else userMsg.selfMsg = false;
+          client.send(JSON.stringify(userMsg));
+        });
+      }
+      //command handling
+      else if(userMsg.type=="command"){
+        const command = userMsg.text;
+        //changename
+        if(command.substring(0,"changename".length)=="changename"){
+          const requestedName = command.substring("changename".length).trim();
+          let message = "Changed name to: " + requestedName;
+          if(requestedName.length < 1){
+            message = "Name too short";
+          }
+          // successfully change name
+          else if(!usernames.includes(requestedName)){
+            replaceUsername(ws.id,requestedName);
+            ws.id = requestedName;
+          }
+          // name taken
+          else{
+            message = "Name already taken!";
+          }
+          ws.send(JSON.stringify(createMsg("server-message",message)));
+        }
+        //help
+        else if(command.substring(0,"help".length)=="help"){
+          const helpMsg = {
+            type: "server-message",
+            text: "Type \'!changename <some_name>\' to change your name! Type \'!users\' for a list of connected users",
+          };
+          ws.send(JSON.stringify(helpMsg));
+        }
+        //users
+        else if(command.substring(0,"users".length)=="users"){
+          let userList="";
+          wss.clients.forEach((client)=>{
+            if(userList.length>0)
+              userList += ", "+client.id;
+            else userList += client.id;
+          });
+          const usersMsg = {
+            type: "server-message",
+            text: "users: " + userList
+          };
+          ws.send(JSON.stringify(usersMsg));
+        }
+      }
+    });
   });
-});
 
 function createMsg(msgType,msgText){
   let msg = {
@@ -121,7 +166,7 @@ setInterval(() => {
       text: new Date().toTimeString()
     }
     client.send(JSON.stringify(msg));
-    });
+  });
 }, 1000);
 
 function makeid() {
@@ -134,6 +179,30 @@ function makeid() {
   return text;
 }
 
+// called when user connects for first time
+function makeUsername(){
+  let numTries = 0;
+  const maxTries = 50;
+  let cont = true;
+  let newUsername = null;
+  // check taken usernames
+  while((usernames.includes(newUsername) || cont ) && numTries < maxTries){
+    if(usernames.includes(newUsername)){
+    }
+    numTries++;
+    if(cont==true) cont=false;
+
+    const randNameNum = Math.floor(Math.random() * names.length);
+    const randAdjectiveNum = Math.floor(Math.random() * adjectives.length);
+    newUsername = adjectives[randAdjectiveNum] + " " + names[randNameNum];
+  }
+  if(numTries < maxTries){
+    usernames.push(newUsername);
+  }
+  else ws.close();
+  return newUsername;
+}
+
 function sendAll(msgType, msgText){
   const msg = {
     type: msgType,
@@ -144,14 +213,24 @@ function sendAll(msgType, msgText){
   });
 }
 function sendAllExcept(msgType, msgText, id){
-const msg = {
+  const msg = {
     type: msgType,
     text: msgText
   };
   wss.clients.forEach((client)=>{
     if(client.id != id)
-    client.send(JSON.stringify(msg));
+      client.send(JSON.stringify(msg));
   });
+}
+
+function replaceUsername(originalName, replacement){
+  console.log("removing name:" + originalName);
+  const indexToRemove = usernames.indexOf(originalName);
+  if(indexToRemove > -1){
+    usernames.splice(indexToRemove,1);
+  }
+  usernames.push(replacement);
+
 }
 
 function removeClient(ws){
@@ -159,4 +238,8 @@ function removeClient(ws){
   if(indexToRemove > -1){
     clients.splice(indexToRemove,1);
   }
+}
+
+function log(ws,msg){
+  ws.send(JSON.stringify(createMsg("server-message",msg)));
 }
